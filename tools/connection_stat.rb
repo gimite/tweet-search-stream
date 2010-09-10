bb6988dfa42308efb9371ba8dcc67e9736274ac7
Hiroshi Ichikawa <gimite@gmail.com>
2010-09-10 23:30:01 +0900

    Adding tool to count connections.

---
+++ tools/connection_stat.rb
@@ -0,0 +1,13 @@
+conns = 0
+File.foreach("log/tss_server.output") do |line|
+  if line =~ /Connection accepted: (-?\d+)/
+    conns += 1
+    p conns
+  elsif line =~ /Connection closed: (-?\d+)/
+    conns -= 1
+    p conns
+  elsif line =~ /WebSocket Server is running/
+    conns = 0
+    p conns
+  end
+end
