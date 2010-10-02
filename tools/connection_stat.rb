7f23da4323313c8e82d9317d9ca382d656267d23
Hiroshi Ichikawa <gimite@gmail.com>
2010-10-02 15:02:00 +0900

    Adding time to output of connection_stat.rb.

--- tools/connection_stat.rb
+++ tools/connection_stat.rb
@@ -4,16 +4,22 @@
 # Copyright: Hiroshi Ichikawa <http://gimite.net/en/>
 # License: New BSD License
 
+require "time"
+
 conns = 0
+time = nil
 File.foreach("log/tss_server.output") do |line|
+  if line =~ /^I, \[(\S+)/
+    time = Time.parse($1)
+  end
   if line =~ /Connection accepted: (-?\d+)/
     conns += 1
-    p conns
+    p [time, conns]
   elsif line =~ /Connection closed: (-?\d+)/
     conns -= 1
-    p conns
+    p [time, conns]
   elsif line =~ /WebSocket Server is running/
     conns = 0
-    p conns
+    p [time, conns]
   end
 end
